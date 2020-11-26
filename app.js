14af269d8982bcb9eb156ee30d54ecd7140e61da
clearwater <clearwater92@naver.com>
2020-11-26 21:48:49 +0900

    footer, basic modal

--- app.js
+++ app.js
@@ -32,3 +32,29 @@ function next(direction) {
     slides[index].classList.add("active");
 
 }
+
+//Modal
+var modal = document.querySelector('.contact-form');
+document.querySelector('#open').addEventListener('click', function(){
+    modal.classList.remove('hidden');
+});
+
+document.querySelector('.close-modal').addEventListener('click',function(){
+    modal.classList.add('hidden');
+});
+
+/*
+const contactBtn = document.getElementById('open');
+const modal = document.querySelector('.contact-form');
+const overlay = modal.querySelector('.contact-form__overlay');
+const closeBtn = modal.querySelector('.close-modal');
+const openModal = () => {
+    modal.classList.remove('hidden');
+}
+const closeModal = () => {
+    modal.classList.add('hidden');
+}
+contactBtn.addEventListener('click', openModal);
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+*/
